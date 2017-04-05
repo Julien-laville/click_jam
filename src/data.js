f1272072cb390185d397f821276dfb506a62f344
Julien-laville <alternative451@myopera.com>
2017-04-05 20:00:41 +0200

    add data

---
+++ src/data.js
@@ -0,0 +1,7 @@
+var ressourcesPositions = [
+    new v2d(screenWidth / 2, screenHeight/2),
+    new v2d(screenWidth / 8, screenHeight/8),
+    new v2d(screenWidth - 200, screenHeight/3),
+    new v2d(screenWidth / 3, screenHeight/3),
+    new v2d(screenWidth / 4, screenHeight-300)
+]
